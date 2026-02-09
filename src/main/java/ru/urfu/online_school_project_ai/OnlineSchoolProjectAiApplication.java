d3f791ce616c5d3ca72ff2d88af3127f9ee37347
Stulov Konstantin <stulov.kostya@gmail.com>
2026-02-09 22:47:24 +0500

    Добавлены файлы конфигурации

---
+++ src/main/java/ru/urfu/online_school_project_ai/OnlineSchoolProjectAiApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.online_school_project_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolProjectAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSchoolProjectAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/online_school_project_ai/OnlineSchoolProjectAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.online_school_project_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolProjectAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
