703f54fe857909f8e655c0e6a5c6a7173cc7b7dc
Stulov Konstantin <stulov.kostya@gmail.com>
2026-02-09 23:47:42 +0500

    Добавлены файлы конфигурации

--- src/main/java/ru/urfu/online_school_project_ai/OnlineSchoolProjectAiApplication.java
+++ src/main/java/ru/urfu/online_school_project_ai/OnlineSchoolProjectAiApplication.java
@@ -3,11 +3,15 @@ package ru.urfu.online_school_project_ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Главный класс приложения для онлайн-школы с искусственным интеллектом.
+ * Этот класс запускает Spring Boot приложение.
+ */
 @SpringBootApplication
 public class OnlineSchoolProjectAiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineSchoolProjectAiApplication.class, args);
     }
-
 }
